def27d99beb42431dd653c016c80ffd40ebf90fc
DuhProgrammer13 <duhvanry@gmail.com>
2015-04-03 12:36:13 -0600

    Initial commit

---
+++ core/src/com/desitum/shveetlife/screens/MenuScreen.java
@@ -0,0 +1,7 @@
+package com.desitum.shveetlife.screens;
+
+/**
+ * Created by dvan6234 on 4/3/2015.
+ */
+public class MenuScreen {
+}
---
+++ desktop/src/com/desitum/shveetlife/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.desitum.shveetlife.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.desitum.shveetlife.ShveetLife;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShveetLife(), config);
+	}
+}
