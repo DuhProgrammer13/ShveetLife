e85644aab3ef9a185c778bf255fce61091b4a45c
DuhProgrammer13 <duhvanry@gmail.com>
2015-04-05 09:59:35 -0600

    Added the libraries and started work on MenuButton in Objects

--- core/src/com/desitum/shveetlife/objects/MenuButton.java
+++ core/src/com/desitum/shveetlife/objects/MenuButton.java
@@ -1,7 +1,17 @@
 package com.desitum.shveetlife.objects;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by Zmyth97 on 4/3/2015.
  */
-public class MenuButton {
+public class MenuButton extends Sprite {
+
+    private Texture baseText;
+    private Texture clickText;
+
+    public MenuButton(Texture text, Texture clickText, float x, float y, float width, float height){
+
+    }
 }
